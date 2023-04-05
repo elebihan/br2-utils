ee7ab1c8f10a364e8d5c6195a19512fe8cfaf6d4
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2023-04-06 00:03:23 +0200

    br2-clerk: add command to build a system with a defconfig

--- src/bin/br2-clerk.rs
+++ src/bin/br2-clerk.rs
@@ -55,9 +55,19 @@ pub fn main() -> Result<()> {
 
 mod topics {
     pub mod defconfig {
-        use br2_utils::{Buildroot, Error};
+        use br2_utils::{BuildMode, Buildroot, Error};
         use clap::{Args, Subcommand};
-        use std::collections::BTreeSet;
+        use std::{collections::BTreeSet, path::PathBuf};
+
+        #[derive(Debug, Args)]
+        struct BuildArgs {
+            #[arg(short, long, help = "Build mode", default_value_t = BuildMode::Full)]
+            mode: BuildMode,
+            #[arg(help = "Name of the defconfig")]
+            name: String,
+            #[arg(help = "Path to output directory")]
+            output: PathBuf,
+        }
 
         #[derive(Debug, Args)]
         struct GetArgs {
@@ -69,6 +79,9 @@ mod topics {
 
         #[derive(Debug, Subcommand)]
         enum DefconfigCommand {
+            /// Build an embedded system using a defconfig
+            #[clap(visible_alias = "b")]
+            Build(BuildArgs),
             /// Get value of a symbol
             #[clap(visible_alias = "g")]
             Get(GetArgs),
@@ -86,6 +99,9 @@ mod topics {
         impl Defconfig {
             pub fn execute(&self, buildroot: &Buildroot) -> Result<(), Error> {
                 match self.command {
+                    DefconfigCommand::Build(ref args) => {
+                        buildroot.build_defconfig(&args.name, &args.output, args.mode)
+                    }
                     DefconfigCommand::Get(ref args) => {
                         let defconfig = buildroot.get_defconfig(&args.name)?;
                         let symbol = defconfig
